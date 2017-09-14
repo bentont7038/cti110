de03d9b99f3be27013b74931cdd77b0e9e23ee80
bentont7038 <31968239+bentont7038@users.noreply.github.com>
2017-09-14 14:24:12 -0400

    Add files via upload
    First submission

---
+++ M2T1_SalesPrediction_Benton.py
@@ -0,0 +1,7 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Terrel Benton
+# 09/07/2017
+total_sales = int(input("What are your total sales:"))
+annual_profit = total_sales * 0.28
+print("Your annual profit is $", annual_profit)
